02f09332d2f5d949e06f40b72ae8b372ea734ff5
Stuparu Mihai <mihai10shiros@yahoo.com>
2016-02-29 23:25:34 +0200

    Adding types for drinks as soda,beer and rum

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 $('.mancare').on('mouseover', function() {
 	var imagineId = $(this).attr('data-li');
-	$('#'+imagineId).show(300);
+	$('#'+imagineId).show(300); 
 	$(this).find('span').show();
 });
 
@@ -12,6 +12,22 @@ $('.mancare').on('mouseleave', function() {
 	var imagineId = $(this).attr('data-li');
 	$('#'+imagineId).hide(100);
 	$(this).find('span').hide();
+});
+//pentru categoriile de bauturi
+$('.tab-panels .tabs li').on('click', function() { //cauta li in clasa .tabs
+	var panel = $(this).closest('.tab-panels'); //verifica panel-ul in care se afla
+	panel.find('.tabs li.active').removeClass('active'); //scoate clasa active
+	$(this).addClass('active'); //adauga active pe cel pe care am dat click
+	var panelShow = $(this).attr('rel'); //afla panel-ul pe care am dat click
+
+	panel.find('.panel.active').slideUp(300, showNextPanel); //ascunde pe cel curent
+
+	 function showNextPanel() { //arata urmatorul panel
+		$(this).removeClass('active'); //sterge clasa active
+		$('#'+panelShow).slideDown(300 , function() { //arata panel-ul
+			$(this).addClass('active'); //adauga clasa activa
+		});			
+	}
 });
 
 $('.mancare').on('click', function() {  //?????
@@ -21,17 +37,14 @@ $('.mancare').on('click', function() {  //?????
 
 $('.co').find('ul').delegate('#butonx', 'click', function(e) {
 	var remove = $(e.target).closest('li');
-	$(remove).remove();
+	remove.remove();
 	$('.co').find('ul').append('<li id="comanda"></li>');
 });
 
 
-
-
-
 $('.btn').on('click', function() {  //?????
 	var string = $('#comanda').find('li');
-		var numar = $(string).children('span');
+		var numar = string.children('span');
 		//var thenum = thestring.replace( /^\D+/g, '');
         alert(numar);
 
