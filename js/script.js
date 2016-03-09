e50a770fb83c6d093a497971218cf0437991944e
Stuparu Mihai <mihai10shiros@yahoo.com>
2016-03-09 23:25:46 +0200

    Js reconstruction

--- js/script.js
+++ js/script.js
@@ -1,101 +1,64 @@
-console.log('Loaded script.js');
 
-$(document).ready(function () {
+$(document).ready( function () {
 
-	$('.mancare').on('mouseover', function (event) {
 
-		event.preventDefault();
+	$('li').on('mouseover', function () { //show image
+
+		var atr = $(this).attr('data-li');
+		var set = $('#' + atr).closest('.imaginep');
+
 		$(this).find('span').show();
-		var imagineId = $(this).attr('data-li');
-		var img = $('#'+imagineId).closest('.imaginep');
-		img.find('.imagine.active').removeClass('active').fadeOut(1000, showNextImg);
+		set.find('.active').removeClass('active').fadeOut(100, nextImg);
 
-		function showNextImg () {
-			$('#'+imagineId).fadeIn(1000); 
-			$('.mancare').find('span').hide();
-			$('#'+imagineId).addClass('active');				
-		}		
+		function nextImg () {
+
+			$('#' + atr).fadeIn(1000);
+			$('#' + atr).addClass('active');
+			$('li').find('span').hide();
+
+		}
+		
 	});
 
-	$('.tab-panels .tabs li').on('click', function (event) { 
-		
-		event.preventDefault();
-		var panel = $(this).closest('.tab-panels'); 
-		panel.find('.tabs li.active').removeClass('active'); 
-		$(this).addClass('active');
-		var panelShow = $(this).attr('rel'); 
-		panel.find('.panel.active').slideUp(300, showNextPanel); 
+	$('.imagine').hover( function () { //image hover
 
-	 	function showNextPanel () { 
+		$('.panel').css("opacity","0.6");
+		$(this).css({
+					"opacity" : "1",
+					"border" : "2px solid #fff",
+					"box-shadow" : "10px 10px 5px #ccc"
+		});
+		
+	}, function () {
+
+		$('.panel').css("opacity","1");
+		$(this).css({
+					"opacity" : "0.8",
+					"border" : "none",
+					"box-shadow" : "none"
+		});
+
+	});
+
+	$('.tabs li').on('click', function () { //drinks panel
+
+		var tab = $(this).closest('.tab-panels');
+		var atr = $(this).attr('rel');
+
+		
+		tab.find('.tabs li.active').removeClass('active');
+		$(this).addClass('active');
+		tab.find('.panel.active').slideUp(100, nextPanel);
+
+		function nextPanel () {
+
 			$(this).removeClass('active'); 
-			$('#'+panelShow).slideDown(300 , function () { 
+			$('#' + atr).slideDown(100 , function () { 
 			$(this).addClass('active');
 			});			
 		}
-	});
-
-	$('.pizza .mancare').on('click', function (event) { 
-	
-		event.preventDefault();
-		var $comanda = $("#comanda");
-		var $button = $('.butonx');	
-
-		$comanda.append($(this).clone());
-		$comanda.find('.mancare p').remove();
-		$button.addClass('active');	
-
-		var element = $comanda.find('.mancare h4 span').text();
-		$('.raspuns').text('');
-		$( "#comanda span" ).each(function () {
-  			var ap =$( this ).text().concat('+');  
-  			$('.raspuns').append(ap);
-		});
-
-		var txt = $('.raspuns').text();
-  		var fin = txt.substr(0, txt.length-1);
-  		var sum = (new Function( 'return ' + fin))();
- 		$('.raspuns').text('');
- 		$('.raspuns').append(sum); 	
-	
-	});
-
-	$('#comanda').on('click', '.butonx', function (event) {
-
-		event.preventDefault();
-		var txt = $('.raspuns').text();
-		var ultim = $("#comanda li:last-child").find('span').text();
-		var undo = (new Function( 'return ' + (txt-ultim)))();
-		$('.raspuns').text('');
-		$('.raspuns').append(undo); 
-		$("#comanda li:last-child").remove(); 
-	});
-
-	$('.btn').on('click', function (event) {  
-
-		event.preventDefault();
-		var suma = $('.raspuns').text();
-		var txt = String(suma);
-		var dmin = 10;
-		var pmin = 30;
-    	var fpizza = $("#comanda li h4").hasClass("pizaa");
-    	var fdrinks = $("#comanda li h4").hasClass("drinks");
-
-    	if (fpizza && fdrinks) {
-    		$().toastmessage('showSuccessToast', "You have successfully sent your order! Congratz!");
-    	}
-		else if (txt >= pmin) {
-			$().toastmessage('showSuccessToast', "You have successfully sent your order! Congratz!");
-		}
-		else if (fpizza) {	
-			$().toastmessage('showErrorToast', " The total price should be at least 30 when you choose to buy pizza only!");		
-		}
-		else if (fdrinks && (txt >= dmin)) {
-			$().toastmessage('showSuccessToast', "You have successfully sent your order! Congratz!");
-		}
-		else {
-			$().toastmessage('showErrorToast', "The total price should be at least 10 when you choose to buy drinks only!");
-		} 
 
 	});
+
 
 });
